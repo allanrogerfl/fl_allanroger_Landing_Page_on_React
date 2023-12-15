005a3e5c4dbb91e6d9841cfe48ead4467d5a4bcf
Allan Roger <105656076+allanrogerfl@users.noreply.github.com>
2023-12-15 20:35:45 +0000

    primerCommit

---
+++ src/js/component/cards.jsx
@@ -0,0 +1,54 @@
+import React from "react";
+
+//create your first component
+const Cards = () => {
+  return (
+    <div className="container text-center">
+      <div class="row ">
+      <div className="card text-center col m-4" style={{width: "18rem"}}>
+        <img src="" className="card-img-top" alt="..." />
+        <div className="card-body">
+          <h5 className="card-title">Card title</h5>
+          <p className="card-text">Lorem ipsum dolor sit amet consectetur adipisicing elit. Consequuntur, architecto
+            eius tempora numquam voluptatibus accusamus? Dignissimos
+            magnam quae, velit quasi officia dolorum praesentium ipsa, deserunt sapiente beatae corrupti quod ex!</p>
+          <a href="#" className="btn btn-primary">Find Out More!</a>
+        </div>
+      </div>
+      <div className="card text-center col m-4" style={{width: "18rem"}}>
+        <img src="..." className="card-img-top" alt="..." />
+        <div className="card-body">
+          <h5 className="card-title">Card title</h5>
+          <p className="card-text">Lorem ipsum dolor sit amet consectetur adipisicing elit. Consequuntur, architecto
+            eius tempora numquam voluptatibus accusamus? Dignissimos
+            magnam quae, velit quasi officia dolorum praesentium ipsa, deserunt sapiente beatae corrupti quod ex!</p>
+          <a href="#" className="btn btn-primary">Find Out More!</a>
+        </div>
+      </div>
+      <div className="card text-center col m-4" style={{width: "18rem"}}>
+        <img src="..." className="card-img-top" alt="..." />
+        <div className="card-body">
+          <h5 className="card-title">Card title</h5>
+          <p className="card-text">Lorem ipsum dolor sit amet consectetur adipisicing elit. Consequuntur, architecto
+            eius tempora numquam voluptatibus accusamus? Dignissimos
+            magnam quae, velit quasi officia dolorum praesentium ipsa, deserunt sapiente beatae corrupti quod ex!</p>
+          <a href="#" className="btn btn-primary">Find Out More!</a>
+        </div>
+        </div>
+        <div className="card text-center col m-4" style={{width: "18rem"}}>
+          <img src="..." className="card-img-top" alt="..." />
+          <div className="card-body">
+            <h5 className="card-title">Card title</h5>
+            <p className="card-text">Lorem ipsum dolor sit amet consectetur adipisicing elit. Consequuntur, architecto
+              eius tempora numquam voluptatibus accusamus? Dignissimos
+              magnam quae, velit quasi officia dolorum praesentium ipsa, deserunt sapiente beatae corrupti quod ex!</p>
+            <a href="#" className="btn btn-primary">Find Out More!</a>
+          </div>
+      </div>
+      </div>
+      </div>
+
+      );
+};
+
+      export default Cards;
--- src/js/component/home.jsx
+++ src/js/component/home.jsx
@@ -1,26 +1,29 @@
 import React from "react";
+import Cards from "./cards";
 
 //include images into your bundle
-import rigoImage from "../../img/rigo-baby.jpg";
+// import Cards from "./cards.jsx";
 
 //create your first component
 const Home = () => {
-	return (
-		<div className="text-center">
-			<h1 className="text-center mt-5">Hello Rigo!</h1>
-			<p>
-				<img src={rigoImage} />
-			</p>
-			<a href="#" className="btn btn-success">
-				If you see this green button... bootstrap is working...
-			</a>
-			<p>
-				Made by{" "}
-				<a href="http://www.4geeksacademy.com">4Geeks Academy</a>, with
-				love!
-			</p>
-		</div>
-	);
+  return (
+    <div>
+      <div className="mx-auto" style={{width: "70%"}}>
+        <div className="card bg-light">
+          <div className="card-body">
+            <h1 className="card-title">A Warm Welcome!</h1>
+            <p className="card-text">Lorem ipsum dolor, sit amet consectetur adipisicing elit.
+              Atque eos beatae repellendus itaque libero commodi
+              eveniet labore voluptatum repudiandae. Aliquam quidem unde provident incidunt fugit odio
+              numquam at placeat aliquid?</p>
+            <a href="#" className="btn btn-primary">Call to action!</a>
+          </div>
+        </div>
+        <Cards/>
+      </div>
+    </div>
+
+  );
 };
 
 export default Home;
